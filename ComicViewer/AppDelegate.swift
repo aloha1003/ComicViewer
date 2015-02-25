8699db858876049a4c58a3f5fb30abb47db88667
aloha1003 <knives1003@gmail.com>
2015-02-25 14:49:21 +0800

    Initial Commit

---
+++ ComicViewer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ComicViewer
+//
+//  Created by aloha1003 on 2015/2/25.
+//  Copyright (c) 2015年 aloha1003. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
